6d34052b529249648021cc9d63e73197477c6bf5
Alek Zywot <a1ek@protonmail.ch>
2019-04-28 08:23:35 -0400

    Update PiMonitorSetup.sh

--- Pi/PiMonitorSetup.sh
+++ Pi/PiMonitorSetup.sh
@@ -1,20 +1,76 @@
 #!/bin/bash
-# This script setups Pi Currant to monitor my pi.
-# Im lazy so this is downloaded and autorun on every new pi I setup.
+# Pi setup - Headless
 
-# instll lighttpd
-apt-get install lighttpd php5-cgi --yes
-lighttpd-enable-mod fastcgi fastcgi-php
-service lighttpd force-reload
+# Enabled via boot folder script
+# SSH, WiFi country, & WiFi
+# Set locale: en_US.UTF-8 UTF-8
+# Set time zone to New_York/Eastern (EST)
+sudo timedatectl set-timezone EST
 
-# backup current server
-mv /var/www/html /var/www/html_backup
-mkdir /var/www/html
 
-# download and setup PiCurrant
-cd /tmp/
-wget https://github.com/ColinWaddell/CurrantPi/archive/master.zip -O temp.zip
-unzip temp.zip
-rm temp.zip
-cp -r /tmp/CurrantPi-master/* /var/www/html/
-rm -rf /tmp/CurrantPi-master
+# Boot Optimization
+# Backup Boot/config.txt
+sudo cp /boot/config.txt /boot/config_backup.txt
+# GPU Ram (Min = 16)
+sudo sed -i -e '$a \# # Set GPU Ram \ngpu_mem=16' /boot/config.txt
+# Disable Boot Splash
+sudo sed -i -e '$a \# Disable Boot Splash\ndisable_splash=1' /boot/config.txt
+# Backup Boot/cmdline.txt
+sudo cp /boot/cmdline.txt /boot/cmdline_backup.txt
+#  Enable cgroups in Raspberry pi kernel config
+sudo sed -i -e 's/$/ cgroup_enable=cpuset cgroup_enable=memory/' /boot/cmdline.txt
+
+
+# Power conservation
+# Disable HDMI on boot using rc.local
+sudo sed -i -e '$i \# Disable HDMI to conserve power\n/usr/bin/tvservice -o\n' /etc/rc.local
+echo 'HDMI will be disabled on boot starting with next reboot'
+
+
+# Update packages
+sudo apt-get update
+echo 'Upgrading packages'
+sudo apt-get -y dist-upgrade
+echo ' Done upgrading system'
+
+# pip3 install --upgrade pip
+# sudo apt-get remove python3-pip; sudo apt-get install python3-pip
+
+
+# Install packages
+#Install git
+sudo apt-get -y install git
+
+
+# Install setfacl/acl (Access Control Lists)
+sudo apt-get -y install acl
+
+
+#DOCKER
+# Install Docker & add usergroup
+curl -sSL get.docker.com | sh && \
+  sudo usermod pi -aG docker
+# Install Docker-Compose
+sudo apt-get -y install docker-compose
+# Create Docker directory
+mkdir ~/docker
+sudo setfacl -Rdm g:docker:rwx ~/docker
+sudo chmod -R 775 ~/docker
+
+
+# Install Ram2Log
+# Keep logs in ram and write to sd card every 1 hour
+cd ~
+git clone https://github.com/azlux/log2ram
+cd log2ram
+chmod +x install.sh && sudo ./install.sh
+cd ..
+sudo rm -r log2ram
+# Reboot prior to any other install
+
+# Cleanup git packages
+sudo apt-get clean
+
+# NEEDS REBOOT
+echo 'MUST REBOOT prior to any other install'
+echo 'WILL REBBOT NOW'
